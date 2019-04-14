5f7d623fab390d78c14bae07f8fe263a13663ee3
grejalot <g.rejalot@gmail.com>
2019-04-14 12:05:26 +0200

    Générer le plateau

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,52 @@
 
 $(document).ready(function(){
 
-    var fruits = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18];
 
-    function generer(){
+    function generer_plateau(){ 
+        
+        var fruits = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17];
+
         for (var i=0;i<36;i++){
-            $('.plateau').append('<div class="carte" id="carte_'+i+'"></div>');
+            $('.plateau').append('<div class="carte masquee" id="carte_'+i+'"></div>');
 
-            var fruit = -(fruits[i])*100;
+            var decompte = 36-i;
+            var hasard = Math.floor(Math.random()*Math.floor(decompte));
+            var fruit = fruits[hasard];
 
-            $('#carte_'+i).css("background-position", "center "+fruit+"px");
-            $('#carte_'+i).text(fruits[i]);
+            fruits.splice(hasard, 1);
+
+
+            var fond = -(fruit)*100;
+
+            $('#carte_'+i).css("background-position", "center "+fond+"px");
+            $('#carte_'+i).text(i+" - "+fruit);
+
+            console.log(fruits);
 
         }
     }
-    generer();
 
-    $('.carte').click(function(){
-        var x = $(this).attr('id');
-        alert(x);
+    function rejouer(){
+        $('.carte').remove();
+        generer_plateau();
+    }
 
+
+    generer_plateau();
+
+    $('.plateau').on('click', '.carte', function(){
+        /*var x = $(this).attr('id');
+        alert(x);*/
+
+        if($(this).hasClass("visible")){
+            $(this).removeClass("visible").addClass("masquee");
+        }else{
+            $(this).removeClass("masquee").addClass("visible");
+        }
+    });
+
+    $('#rejouer').click(function(){
+        rejouer();
     });
 
 
